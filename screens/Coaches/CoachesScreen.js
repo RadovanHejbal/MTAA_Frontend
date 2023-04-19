41dd50208a2ef78f75e522143c6268227a29a7aa
RadovanHejbal <hejbalradovan@gmail.com>
2023-04-19 17:15:04 +0200

    mycoaches start

--- screens/Coaches/CoachesScreen.js
+++ screens/Coaches/CoachesScreen.js
@@ -1,15 +1,34 @@
-import { View, Text, StyleSheet, SafeAreaView, Pressable } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, Pressable, FlatList } from "react-native";
 import TopScreen from "../../components/TopScreen";
 import NavBar from "../../components/NavBar";
 import colors from "../../variables/colors";
+import { useEffect, useState, useContext } from "react";
+import axios from 'axios';
+import url from '../../variables/url';
+import { AuthContext} from '../../contextapi/AuthContext';
 
 const CoachesScreen = ({ navigation }) => {
+  const [myCoaches, setMyCoaches] = useState([]);
+  const auth = useContext(AuthContext);
+
+  useEffect(() => {
+    axios.get(`${url}/coaches/relations/${auth.user.id}`).then(response => {
+      setMyCoaches(response.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  })
+
   return (
     <SafeAreaView style={styles.container}>
       <TopScreen navigation={navigation}/>
       <View style={styles.SectionContainer}>
         <Text style={styles.Section}>Coaches</Text>
       </View>
+      <Pressable><Text>Buy a coach!</Text></Pressable>
+      {myCoaches.length > 0 ? <FlatList data={myCoaches} keyExtractor={item => item.id} renderItem={({item}) => {
+        return 
+      }} /> : <Text>You dont have any coach. Try and buy one.</Text>}
       <NavBar navigation={navigation} current="Coaches" />
     </SafeAreaView>
   );
